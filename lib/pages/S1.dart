9131d01c3c0d513ea17e5d491309ae7031cd1b36
sopy <gorhtlfj2835@gmail.com>
2022-02-10 19:02:23 +0900

    3. firebase 데이터 받아오기

--- lib/pages/S1.dart
+++ lib/pages/S1.dart
@@ -1,21 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+final firestore = FirebaseFirestore.instance;
 
 class S1 extends StatefulWidget {
   const S1({Key? key}) : super(key: key);
-
   @override
   _S1State createState() => _S1State();
 }
 
 class _S1State extends State<S1> {
-  static const TextStyle optionStyle =
-      TextStyle(fontSize: 30, fontWeight: FontWeight.w500);
+  var fbData;
+
+  getData() async {
+    var result = await firestore.collection('profile').get();
+    setState(() {
+      fbData = result.docs;
+    });
+    print(fbData);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      "feed",
-      style: optionStyle,
-    );
+    return Text("feed");
   }
 }
