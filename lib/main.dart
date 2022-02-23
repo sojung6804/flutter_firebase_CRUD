f04597a79ad8da24333c72823380e2d0aeef8c92
sopy <gorhtlfj2835@gmail.com>
2022-02-23 21:07:30 +0900

    26. dataLoading 페이지 생성

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutterfirebase/pages/S1.dart';
 import 'package:flutterfirebase/pages/S2.dart';
+import 'package:flutterfirebase/pages/dataLoading.dart';
 import 'package:flutterfirebase/pages/joinPage.dart';
 import 'package:flutterfirebase/store/store1.dart';
 import 'firebase_options.dart';
@@ -31,7 +32,7 @@ class _HomePageState extends State<HomePage> {
   int _currentIndex = 0;
   static const TextStyle optionStyle =
       TextStyle(fontSize: 30, fontWeight: FontWeight.w500);
-  final List<Widget> _children = [joinPage(), S1(), S2()];
+  final List<Widget> _children = [joinPage(), S1(), S2(), dataLoading()];
 
   void onTabTapped(int index) {
     setState(() {
@@ -60,6 +61,10 @@ class _HomePageState extends State<HomePage> {
           BottomNavigationBarItem(
             icon: Icon(Icons.favorite),
             label: 'Profile',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add),
+            label: 'dataLoading',
           ),
         ],
       ),
---
+++ lib/pages/dataLoading.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class dataLoading extends StatefulWidget {
+  const dataLoading({Key? key}) : super(key: key);
+
+  @override
+  _dataLoadingState createState() => _dataLoadingState();
+}
+
+class _dataLoadingState extends State<dataLoading> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
