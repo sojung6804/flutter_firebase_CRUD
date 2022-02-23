63e4ab28a61748cf358f639bfe71c05ff0184d85
sopy <gorhtlfj2835@gmail.com>
2022-02-23 18:21:06 +0900

    24. 로그인 버튼 구현

---
+++ lib/pages/UserInputPage2.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UserInputPage2 extends StatefulWidget {
+  const UserInputPage2({Key? key}) : super(key: key);
+
+  @override
+  _UserInputPage2State createState() => _UserInputPage2State();
+}
+
+class _UserInputPage2State extends State<UserInputPage2> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/pages/loginPage.dart
+++ lib/pages/loginPage.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'UserInputPage2.dart';
+
+final auth = FirebaseAuth.instance;
+final firestore = FirebaseFirestore.instance;
 
 class loginPage extends StatefulWidget {
   const loginPage({Key? key}) : super(key: key);
@@ -8,8 +15,81 @@ class loginPage extends StatefulWidget {
 }
 
 class _loginPageState extends State<loginPage> {
+  var inputID = TextEditingController();
+  var inputPW = TextEditingController();
+  var inputNick = TextEditingController();
+
+  getLogin() async {
+    try {
+      var result = await auth.createUserWithEmailAndPassword(
+          email: "sojung@naver.com", password: "123456");
+      print(result.user);
+    } catch (e) {
+      print(e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: TextField(
+              controller: inputNick,
+              style: TextStyle(fontSize: 25),
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: '닉네임',
+                  hintStyle: TextStyle(color: Colors.grey)),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: TextField(
+              controller: inputID,
+              style: TextStyle(fontSize: 25),
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: '이메일',
+                  hintStyle: TextStyle(color: Colors.grey)),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: TextField(
+              controller: inputPW,
+              style: TextStyle(fontSize: 25),
+              textAlign: TextAlign.center,
+              decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: '패스워드',
+                  hintStyle: TextStyle(color: Colors.grey)),
+            ),
+          ),
+          GestureDetector(
+            behavior: HitTestBehavior.translucent,
+            onTap: () async {
+              getLogin();
+              Navigator.push(context, MaterialPageRoute(builder: (c) {
+                return UserInputPage2();
+              }));
+            },
+            child: Container(
+              padding: EdgeInsets.all(13),
+              decoration: BoxDecoration(
+                  border: Border.all(width: 2, color: Colors.grey),
+                  borderRadius: BorderRadius.circular(12)),
+              child: Text('로그인', style: TextStyle(fontSize: 30)),
+            ),
+          ),
+        ],
+      ),
+    ));
   }
 }
